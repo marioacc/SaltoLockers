f1a6f9c81cb1e011925f0e6b7ecb8c8afa740117
MarioContreras <mario.acc.15@gmail.com>
2015-05-29 12:42:20 -0600

    Bug in money table
    The money table had a bug in the sum

--- Lockers/static/js/app/admin_currency.js
+++ Lockers/static/js/app/admin_currency.js
@@ -12,26 +12,26 @@ $(document).ready(
             items+="<tr id=user_"+content.user_id+">";
             items+="<td>"+content.currency_name+"</td>";
             items+="<td>"+content.currency_quantity+"</td>";
-            if (content.currency_name="50 centavos"){
+            if (content.currency_name=="50 centavos"){
                 moneyByType=content.currency_quantity*.5;
             }
-            else if (content.currency_name="1 peso"){
+            else if (content.currency_name=="1 peso"){
                 moneyByType=content.currency_quantity;
             }
-            else if (content.currency_name="2 pesos"){
+            else if (content.currency_name=="2 pesos"){
                 moneyByType=content.currency_quantity*2;
             }
-            else if (content.currency_name="5 pesos"){
+            else if (content.currency_name=="5 pesos"){
                 moneyByType=content.currency_quantity*5;
             }
-            else if (content.currency_name="10 pesos"){
+            else if (content.currency_name=="10 pesos"){
                 moneyByType=content.currency_quantity*10;
             }
             items+="<td>"+"$"+moneyByType+"</td>";
             items+="</tr>";
 
             totalMoney+=moneyByType;
-            totalTableItems="<tr><td>"+totalMoney+"</td></tr>";
+            totalTableItems="<tr><td>$"+totalMoney+"</td></tr>";
         });
         (document).getElementById("table_body_currency").innerHTML=items;
         (document).getElementById("table_body_total").innerHTML=totalTableItems;
